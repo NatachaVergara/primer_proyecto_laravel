71cd271141d37d23520ec1f9e85c3add07913a39
Natacha Vergara <ntchvergara@gmail.com>
2022-09-14 13:39:52 -0300

    creando las migraciones en la database

--- database/migrations/2022_09_14_155509_crear_tabla_roles.php
+++ database/migrations/2022_09_14_155509_crear_tabla_roles.php
@@ -13,13 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('usuarios', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nombre', 50)->unique();
+            $table->string('slug', 50)->unique();
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('usuarios');
+        Schema::dropIfExists('roles');
     }
 };
--- database/migrations/2022_09_14_163618_crear_tabla_post.php
+++ database/migrations/2022_09_14_163618_crear_tabla_post.php
@@ -13,13 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('usuarios', function (Blueprint $table) {
+        Schema::create('post', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -31,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('usuarios');
+        Schema::dropIfExists('post');
     }
 };
